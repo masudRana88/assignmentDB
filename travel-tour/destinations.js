fae03d49afb6e750ec0fbf5a29644c598ebacc4f
masudRana88 <mahimuddin88@gmail.com>
2021-10-29 01:20:08 +0600

    assingnment demo data

---
+++ travel-tour/destinations.js
@@ -0,0 +1,65 @@
+[
+  {
+    "id": 1,
+    "name": "Venice, Italy",
+    "description": "If traversing the canals with a be-striped gondolier sounds unbearably touristy, stick to the sidewalks and spectacular arched bridges to get your fill of this truly unique, wildly romantic floating city.",
+    "img": "https://user-images.githubusercontent.com/86189450/139319771-fa58094b-d3df-4c81-8b3f-2415cf7e072d.jpg",
+    "price": "2,000"
+  },
+  {
+    "id": 2,
+    "name": "Great Ocean Road, Australia",
+    "descriptino": "Head west from Melbourne on this coastal drive to see everything from the famous 12 Apostle rock formations, to koalas in Great Otway National Park as well as the charming seaside town of Lorne.",
+    "img": "https://user-images.githubusercontent.com/86189450/139319772-c04cb2d5-ceb3-4539-878a-e84aa8d2338f.jpg",
+    "price": "1,600"
+  },
+  {
+    "id": 3,
+    "name": "Pamukkale, Turkey",
+    "descriptino": "Put every infinity pool you’ve ever seen to shame with these natural, snow-white hot springs overlooking the nearby city of Denizli. On top of the picturesque soak, Pamukkale is also home to the impressively preserved ruins of the ancient Roman spa-city Hierapolis where you can bathe like an emperor among the submerged centuries-old columns.",
+    "img": "https://user-images.githubusercontent.com/86189450/139319745-25c532a2-8894-4aa6-9048-da2220d378cf.jpg",
+    "price": "2,400"
+  },
+  {
+    "id": 4,
+    "name": "Pitons, St Lucia",
+    "descriptino": "These two volcanic mountains are one of the most recognizable—not to mention beautiful—features on this Caribbean island. Enjoy the Pitons from the pristine white sand beach at Sugar Beach, A Viceroy Resort.",
+    "img": "https://user-images.githubusercontent.com/86189450/139319752-be489c31-fb16-4c65-9811-ca4379558340.jpg",
+    "price": "1500"
+  },
+  {
+    "id": 5,
+    "name": "Bora Bora, French Polynesia",
+    "descriptino": "With turquoise lagoons, coral reefs and overwater bungalows, this small island in the South Pacific is basically heaven on Earth.",
+    "img": "https://user-images.githubusercontent.com/86189450/139319755-42aa9e5e-310c-47b0-9975-69eacb71d8bd.jpg",
+    "price": "1,300"
+  },
+  {
+    "id": 6,
+    "name": "Oia, Santorini, Greece",
+    "descriptino": "All of the Greek islands are undeniably beautiful, but head to the hillside town of Oia for some of the best views of the bright blue Aegean Sea.",
+    "img": "https://user-images.githubusercontent.com/86189450/139319756-a820bb0c-19e5-4b5f-927c-b16b44edeb31.jpg",
+    "price": "2,999"
+  },
+  {
+    "id": 7,
+    "name": "Torres del Paine National Park, Chile",
+    "descriptino": "If you're looking to get off the grid, head to Chile's Patagonia region for some of the most beautiful mountain views you'll ever see.",
+    "img": "https://user-images.githubusercontent.com/86189450/139319758-e495fc5d-170e-458f-bf0d-3db448fef668.jpg",
+    "price": "2,999"
+  },
+  {
+    "id": 8,
+    "name": "Forbidden City, Beijing, China",
+    "descriptino": "Located in the center of Beijing, the Forbidden City was the imperial palace of China's emperors for five centuries and is one of the most beautifully preserved examples of ancient Chinese architecture",
+    "img": "https://user-images.githubusercontent.com/86189450/139319761-d1a634a9-dbb0-44b5-8d87-562e0213771e.jpg",
+    "price": "1,850"
+  },
+  {
+    "id": 9,
+    "name": "Krabi, Thailand",
+    "descriptino": "Located on southern Thailand's west coast, this province's coastline is dotted with limestone cliffs and hundreds of islands just offshore in the Andaman Sea, making it one of the world's most popular beach vacation destinations.",
+    "img": "https://user-images.githubusercontent.com/86189450/139319766-8bdaa9ad-3224-4261-afaa-6f5a3de02e4a.jpg",
+    "price": "1,200"
+  }
+]
